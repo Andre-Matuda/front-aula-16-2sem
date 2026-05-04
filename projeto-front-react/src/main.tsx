2853310593a6109cef282215c78d7a415fd66095
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-05-04 10:58:53 -0300

    feat : Principio de modularização e criando o componente aluno.

--- projeto-front-react/src/main.tsx
+++ projeto-front-react/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import Home from "./routes/Home/index.tsx";
 import Users from "./routes/Users/index.tsx";
 import Error from "./routes/Error/index.tsx";
+import Alunos from "./routes/Alunos/index.tsx";
 import { createBrowserRouter,RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <Home /> },
       { path: "/users", element: <Users /> },
+      { path: "/alunos", element: <Alunos /> },
     ],
   },
 ]);
---
+++ projeto-front-react/src/routes/Alunos/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Alunos() {
+  return (
+    <main>
+        <h1>Alunos</h1>
+    </main>
+  )
+}

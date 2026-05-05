ddd552a1ae220b7c00cf5c7e708ecb77a4a3f60f
alecarlosjesus <profalexandre.jesus@email.com.br>
2026-05-05 11:22:55 -0300

    feat : Recuperando o objeto em um novo componente com metódo GET.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@email.com.br>

--- projeto-front-react/src/routes/EditarAluno/index.tsx
+++ projeto-front-react/src/routes/EditarAluno/index.tsx
@@ -1,13 +1,42 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import type { TipoAluno } from "../../types/tipoAluno";
 
 export default function EditarAluno() {
 
     const {id} = useParams<string>();
 
+    
+  const [aluno, setAluno] = useState<TipoAluno>();
+
+  useEffect(() => {
+
+    const callAluno = async ()=>{
+      try {
+        const response = await fetch(`http://localhost:3000/alunos/${id}`);
+        
+        if(response.ok){
+          const data:TipoAluno = await response.json();
+          setAluno(data);
+        }else{
+          throw new Error("Aluno inexistente");
+        }
+
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    callAluno();
+    
+  }, []);
+
   return (
     <main>
         <h1>Editar Aluno</h1>
-        <p>ID: {id}</p>
+        <p>ID: {aluno?.id}</p>
+        <p>RM: {aluno?.rm}</p>
+        <p>ALUNO: {aluno?.aluno}</p>
     </main>
   )
 }

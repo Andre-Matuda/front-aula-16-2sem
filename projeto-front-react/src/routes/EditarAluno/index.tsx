84654fd29ea624d3552a8e74275e12ed91575773
alecarlosjesus <profalexandre.jesus@email.com.br>
2026-05-05 11:16:42 -0300

    feat : Recuperando parâmetro no envio de dados com o hook useParams.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@email.com.br>

--- projeto-front-react/src/routes/EditarAluno/index.tsx
+++ projeto-front-react/src/routes/EditarAluno/index.tsx
@@ -1,8 +1,13 @@
+import { useParams } from "react-router-dom";
 
 export default function EditarAluno() {
+
+    const {id} = useParams<string>();
+
   return (
     <main>
         <h1>Editar Aluno</h1>
+        <p>ID: {id}</p>
     </main>
   )
 }
